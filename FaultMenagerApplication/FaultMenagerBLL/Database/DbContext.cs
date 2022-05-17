4eb16ec7836747915e61fa45632b5659079bdc8b
Bartosz Sadowski <bshadowsky@gmail.com>
2022-05-17 20:09:42 +0200

    Added project with Bisnes Logic

---
+++ FaultMenagerApplication/FaultMenagerBLL/Database/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultMenagerBLL.Database
+{
+    internal class DbContext
+    {
+    }
+}
